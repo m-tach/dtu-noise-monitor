2a5ceaf729d39a570ce69e28deb890312197d0e5
mariya <mariya.tacheva@gmx.com>
2020-02-27 11:17:44 +0100

    contract for how the message will look; needs to be moved out

--- libraries/CommunicationController/CommunicationController.cpp
+++ libraries/CommunicationController/CommunicationController.cpp
@@ -14,3 +14,16 @@ void sendMessage(char * message)
 {
 	DEBUG_STREAM.write(message);
 }
+
+
+//TODO: add to readme; explain each field;
+//TODO: add to a Shared library
+NoiseMonitorMessage
+{
+	uint32_t Timestamp;
+	uint8_t BatteryVoltage;
+	int8_t BoardTemperature;
+	uint32_t Lat;
+	uint32_t Long;
+	uint32_t Noise;
+}
